31785baaa29d4697a402bf7eb423df3601c9dc43
Hyun Suk Shin (MTC SEATTLE) <hyssh@microsoft.com>
2019-03-14 12:49:59 -0700

    add adx queries

---
+++ ADX/createtbl.sql
@@ -0,0 +1,9 @@
+.create table devicelog (
+    captureddatetime: datetime,
+    eventtype: string,
+    bagid: string,
+    partnerid:string,
+    employeeid:string,
+    storeid:string,
+    pipeid:string
+    )
